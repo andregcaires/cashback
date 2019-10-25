669068b0726830fb12541cd9c31bab3e8c1a6093
andregcaires <andregcaires@outlook.com>
2019-10-25 20:21:03 -0300

    Project Created

---
+++ Infra/Utilities/MethodExtensions.cs
@@ -0,0 +1,15 @@
+namespace Utilities
+{
+    public class MethodExtensions
+    {
+        public static T ConvertTo(T1 source, T target) 
+        {
+            var result = Activator.CreateInstance(target);
+            foreach () {
+
+            }
+
+            return (T) result;
+        }
+    }
+}
